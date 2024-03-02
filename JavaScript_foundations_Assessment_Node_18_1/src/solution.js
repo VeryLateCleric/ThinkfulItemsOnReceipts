438a2ce4198d69505c1c46020cca042b9a179b0e
Kenneth <kennethyoung.2960@gmail.com>
2024-03-01 22:41:32 -0600

    6 of 10 tests passed

--- JavaScript_foundations_Assessment_Node_18_1/src/solution.js
+++ JavaScript_foundations_Assessment_Node_18_1/src/solution.js
@@ -17,19 +17,31 @@ function printablePrice(priceInCents) {
   return `$${amount}`;
 }
 
-// Three parameters: products, name, size
-// search through all products and find match both name and size
-// If no for either reason, return null
+// Three parameters: products, name, size // search through all products and find match both name and size // If no for either reason, return null
 function chooseItemByNameAndSize(products, name, size) {
   const product = products.find(item => item.name === name);
-  // if statement, neither product found return null. 'if product false or p.size false'
+  // if statement, neither product found return null. 'if product false OR ' || ' p.size false'
   if (!product || !product.availableSizes.includes(size)) {
     return null;
   }
   return product
 }
 
-function addProductToCart() {}
+// parameters are product and cart. Default cart is an empty object '= {}'
+// if only called with product make new cart
+function addProductToCart(product, cart = {}) {
+  const productName = product.name;
+// check if item does *not* exist in cart. if true, make item an object '= {}' with quantity and price 
+  if (!cart[productName]) {
+    cart[productName] = {
+      quantity: 1,
+      priceInCents: product.priceInCents
+    }; // else if the item does exist in the cart, add one to quantity
+  } else { 
+    cart[productName].quantity += 1;
+  }
+  return(cart);
+}
 
 function calculateTotal() {}
 
